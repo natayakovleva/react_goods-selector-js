adaa55e60f944a8e82b630913a99b9c7ce83a94d
Natalia Yakovleva <yakovlevane29@gmail.com>
2026-01-14 11:24:07 +0200

    add key

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,7 @@ export const App = () => {
 
             return (
               <tr
+                key={good}
                 data-cy="Good"
                 className={isSelected ? 'has-background-success-light' : ''}
               >
